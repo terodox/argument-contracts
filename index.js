4417b340b160501a60bd6b89cf85cac398c795be
terodox <terodox@gmail.com>
2018-04-12 20:01:06 -0400

    Create ArgumentContracts:'

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-modules.exports = class ArgumentContracts {
-    
+function isTypeMatch(argument, type) {
+    if (type == String) {
+        return typeof argument == 'string' || argument instanceof String;
+      }
+
+      if (type == Number) {
+        return typeof argument == 'number' || argument instanceof Number;
+      }
+
+      if (type == Function) {
+        return typeof argument == 'function' || argument instanceof Function;
+      }
+
+      if (type == Object) {
+        return argument !== null && typeof argument == 'object';
+      }
+
+      if (type == Boolean) {
+        return typeof argument == 'boolean';
+      }
+
+      if (typeof Symbol != 'undefined' && type == Symbol) {
+        return typeof argument == 'symbol';
+      }
+
+      return argument instanceof type;
+}
+
+module.exports = class ArgumentContracts {
+    static assertArray(argument, argumentName) {
+        if(!Array.isArray(argument)) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be a string. Value received: ${argument}`);
+        }
+    }
+
+    static assertArrayOf(argument, type, argumentName) {
+        ArgumentContracts.assertArray(argument, argumentName);
+        if(!argument.every(item => isTypeMatch(item, type))) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be an array of ${type}. Value received: ${argument}`);
+        }
+    }
+
+    static assertFunction(argument, argumentName) {
+        if(!isTypeMatch(argument, Function)) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be a function. Value received: ${argument}`);
+        }
+    }
+
+    static assertNumber(argument, argumentName) {
+        if(!isTypeMatch(argument, Number)) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be a number. Value received: ${argument}`);
+        }
+    }
+
+    static assertString(argument, argumentName) {
+        if(!isTypeMatch(argument, String)) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be a string. Value received: ${argument}`);
+        }
+    }
+
+    static assertType(argument, type, argumentName) {
+        if(!isTypeMatch(argument, type)) {
+            throw new TypeError(`Expected ${argumentName || 'argument'} to be a ${type}. Value received: ${argument}`);
+        }
+    }
 }
---
+++ index.spec.js
@@ -0,0 +1,259 @@
+const ArgumentContracts = require('./index');
+
+describe('ArgumentContracts', () => {
+    it('should exist', () => {
+        expect(ArgumentContracts).toEqual(expect.any(Function));
+    });
+
+    describe('assertString', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertString).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a string', () => {
+            expect(() => ArgumentContracts.assertString(null)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString(undefined)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString(1234)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString({})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString([])).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString(()=>{})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertString(class thing {})).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a string and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertString(null, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is a string', () => {
+            expect(() => ArgumentContracts.assertString('The string you need')).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a String', () => {
+            expect(() => ArgumentContracts.assertString(new String('The string you need'))).not.toThrow();
+        });
+    });
+
+    describe('assertNumber', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertNumber).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a number', () => {
+            expect(() => ArgumentContracts.assertNumber(null)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber(undefined)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber('Some stringy thingy')).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber({})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber([])).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber(()=>{})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertNumber(class thing {})).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a number and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertNumber(null, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is a number', () => {
+            expect(() => ArgumentContracts.assertNumber(1234)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Number', () => {
+            expect(() => ArgumentContracts.assertNumber(new Number(1234))).not.toThrow();
+        });
+    });
+
+    describe('assertFunction', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertFunction).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a function', () => {
+            expect(() => ArgumentContracts.assertFunction(null)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertFunction(undefined)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertFunction('Some stringy thingy')).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertFunction({})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertFunction([])).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertFunction(1234)).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a function and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertFunction(null, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is a function', () => {
+            expect(() => ArgumentContracts.assertFunction(() => {})).not.toThrow();
+            expect(() => ArgumentContracts.assertFunction(new Function())).not.toThrow();
+        });
+    });
+
+    describe('assertArray', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertArray).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a array', () => {
+            expect(() => ArgumentContracts.assertArray(null)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray(undefined)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray('Some stringy thingy')).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray({})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray(1234)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray(()=>{})).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArray(class thing {})).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a array and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertArray(null, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is a array', () => {
+            expect(() => ArgumentContracts.assertArray([])).not.toThrow();
+        });
+    });
+
+    describe('assertArrayOf', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertArrayOf).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a array', () => {
+            expect(() => ArgumentContracts.assertArrayOf(null, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArrayOf(undefined, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArrayOf({}, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArrayOf(1234, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArrayOf(()=>{}, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertArrayOf(class thing {}, String)).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a array and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertArrayOf(null, String, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is an array of String', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ "Yay strings" ], String)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of String', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ new String("Yay strings") ], String)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Number', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ 1234 ], Number)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Number', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ new Number(1324) ], Number)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Function', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ () => {} ], Function)).not.toThrow();
+            expect(() => ArgumentContracts.assertArrayOf([ function () {} ], Function)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Function', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ new Function(() => {}) ], Function)).not.toThrow();
+        });
+
+        it('should throw if argument is an array of Object, but contains a null', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ null ], Object)).toThrowError(/Expected/);
+        });
+
+        it('should NOT throw if argument is an array of Object', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ {} ], Object)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Object', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ new Object() ], Object)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Boolean', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ true ], Boolean)).not.toThrow();
+            expect(() => ArgumentContracts.assertArrayOf([ false ], Boolean)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of Symbol', () => {
+            expect(() => ArgumentContracts.assertArrayOf([ Symbol() ], Symbol)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an array of a specific type', () => {
+            class TheTypeExists {}
+
+            expect(() => ArgumentContracts.assertArrayOf([ new TheTypeExists() ], TheTypeExists)).not.toThrow();
+        });
+    });
+
+    describe('assertType', () => {
+        it('should be a static method', () => {
+            expect(ArgumentContracts.assertType).toEqual(expect.any(Function));
+        });
+
+        it('should throw if argument is not a array', () => {
+            expect(() => ArgumentContracts.assertType(null, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertType(undefined, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertType({}, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertType(1234, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertType(()=>{}, String)).toThrowError(/Expected/);
+            expect(() => ArgumentContracts.assertType(class thing {}, String)).toThrowError(/Expected/);
+        });
+
+        it('should throw error with argumentName if argument is not a array and argument name is provided', () => {
+            const argumentName = 'thatThingYouLike';
+            expect(() => ArgumentContracts.assertType(null, String, argumentName)).toThrowError(new RegExp(argumentName));
+        });
+
+        it('should NOT throw if argument is a String', () => {
+            expect(() => ArgumentContracts.assertType( "Yay strings" , String)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a String', () => {
+            expect(() => ArgumentContracts.assertType( new String("Yay strings") , String)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Number', () => {
+            expect(() => ArgumentContracts.assertType( 1234 , Number)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Number', () => {
+            expect(() => ArgumentContracts.assertType( new Number(1324) , Number)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Function', () => {
+            expect(() => ArgumentContracts.assertType( () => {} , Function)).not.toThrow();
+            expect(() => ArgumentContracts.assertType( function () {} , Function)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Function', () => {
+            expect(() => ArgumentContracts.assertType( new Function(() => {}) , Function)).not.toThrow();
+        });
+
+        it('should throw if argument is an Object, but contains a null', () => {
+            expect(() => ArgumentContracts.assertType( null , Object)).toThrowError(/Expected/);
+        });
+
+        it('should NOT throw if argument is an Object', () => {
+            expect(() => ArgumentContracts.assertType( {} , Object)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an Object', () => {
+            expect(() => ArgumentContracts.assertType( new Object() , Object)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is an Boolean', () => {
+            expect(() => ArgumentContracts.assertType( true , Boolean)).not.toThrow();
+            expect(() => ArgumentContracts.assertType( false , Boolean)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a Symbol', () => {
+            expect(() => ArgumentContracts.assertType( Symbol() , Symbol)).not.toThrow();
+        });
+
+        it('should NOT throw if argument is a a specific type', () => {
+            class TheTypeExists {}
+
+            expect(() => ArgumentContracts.assertType( new TheTypeExists() , TheTypeExists)).not.toThrow();
+        });
+    });
+});
