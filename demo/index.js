202ca1c64383aed1bc414d70ba0d20c4dfd6c92d
terodox <terodox@gmail.com>
2018-04-15 12:11:20 -0400

    Bring in a demo to show example usage

---
+++ demo/index.js
@@ -0,0 +1,28 @@
+const ArgumentContracts = require('../index');
+
+function consoleLogErrors(throwingFunc) {
+    try {
+        throwingFunc();
+    } catch(error) {
+        console.error(error); // eslint-disable-line no-console
+    }
+}
+
+class SomeMagicalType {}
+
+const NUMBER_VALUE = 1234;
+const STRING_VALUE = '1234';
+consoleLogErrors(() => ArgumentContracts.assertArray(NUMBER_VALUE));
+consoleLogErrors(() => ArgumentContracts.assertArray(NUMBER_VALUE, 'arrayArgument'));
+consoleLogErrors(() => ArgumentContracts.assertArrayOf(NUMBER_VALUE, String));
+consoleLogErrors(() => ArgumentContracts.assertArrayOf(NUMBER_VALUE, String, 'arrayOfStringArgument'));
+consoleLogErrors(() => ArgumentContracts.assertArrayOf([ NUMBER_VALUE ], String));
+consoleLogErrors(() => ArgumentContracts.assertArrayOf([ NUMBER_VALUE ], String, 'arrayOfStringArgument'));
+consoleLogErrors(() => ArgumentContracts.assertFunction(NUMBER_VALUE));
+consoleLogErrors(() => ArgumentContracts.assertFunction(NUMBER_VALUE, 'functionArgument'));
+consoleLogErrors(() => ArgumentContracts.assertNumber(STRING_VALUE));
+consoleLogErrors(() => ArgumentContracts.assertNumber(STRING_VALUE, 'numberArgument'));
+consoleLogErrors(() => ArgumentContracts.assertString(NUMBER_VALUE));
+consoleLogErrors(() => ArgumentContracts.assertString(NUMBER_VALUE, 'stringArgument'));
+consoleLogErrors(() => ArgumentContracts.assertType(STRING_VALUE, SomeMagicalType));
+consoleLogErrors(() => ArgumentContracts.assertType(STRING_VALUE, SomeMagicalType, 'somMagicalTypeArgument'));
