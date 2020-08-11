e3cadbc9ffeb056e7e8fa55a0037ae335a7bdc10
terodox <terodox@gmail.com>
2020-08-11 12:55:22 -0400

    Add safety for contract of argName

--- src/arg-name.spec.ts
+++ src/arg-name.spec.ts
@@ -1,6 +1,11 @@
 import { argName } from './arg-name';
 
 describe('argName', () => {
+    it('should throw if not provided an object', () => {
+        const superFunName = '1234';
+        expect(() => argName(superFunName)).toThrowError('of type "object"');
+    });
+
     it('should return argument name passed as object', () => {
         const superFunName = '1234';
         expect(argName({ superFunName })).toEqual('superFunName');
--- src/arg-name.ts
+++ src/arg-name.ts
@@ -1,3 +1,6 @@
 export function argName(input) {
+    if (typeof input !== "object") {
+        throw new TypeError('The provided input must be of type "object"');
+    }
     return Reflect.ownKeys(input).shift();
 }
